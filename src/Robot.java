0d291cf53a7b187fb5aebb3e0763ddc6de765f47
YeonwooSung <neos960518@gmail.com>
2019-02-12 04:39:18 +0900

    add some javadocs

--- src/Robot.java
+++ src/Robot.java
@@ -2,7 +2,10 @@ import easyGui.EasyGui;
 
 public class Robot {
 	private final EasyGui gui;
-		
+
+	/**
+	 * This constructor sets up the basic settings of the EasyGui instance.
+	 */
 	public Robot() {
 		gui = new EasyGui(100,100);
 		
@@ -11,25 +14,33 @@ public class Robot {
 		gui.addButton(1, 0, "RRT", this, "rrt");
 		
 		gui.addButton(1, 1, "Potential Fields", this, "potentialFields");
-		
 	}
 
+	/**
+	 * This method displays the GUI to run the project.
+	 */
 	public void runProject() {
 		gui.show();
 	}
 
+	/**
+	 * Runs the robot with RRT type instance.
+	 */
 	public void rrt() {
 		gui.hide();
 		RRT rt = new RRT();
 		rt.runRobot();
 	}
 
+	/**
+	 * Runs the robot with PotentialFields type instance.
+	 */
 	public void potentialFields() {
 		gui.hide();
 		PotentialFields fields = new PotentialFields();
 		fields.runRobot();
 	}
-	
+
 	// MAIN
 	public static void main(String[] args) {
 		Robot robot = new Robot();
